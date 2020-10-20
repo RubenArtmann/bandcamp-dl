55c705adb4399340452a6fdcad4747a27f0f4741
RubenArtmann <69248730+RubenArtmann@users.noreply.github.com>
2020-10-21 00:11:19 +0200

    Add files via upload

--- mod.ts
+++ mod.ts
@@ -31,7 +31,7 @@ const dump = (url: string,msg="")=>{
 
 while(urlPool.length>0) {
 	let url = urlPool.shift() as string;
-	let result = url.match(/^https?:\/\/([^.]+).bandcamp.com\/?(?:(album|track)\/([^#\/?]+))?(\#[\s\S]*)?$/);
+	let result = url.match(/^https?:\/\/([^.]+)\.bandcamp\.com\/?(artists|album|track)?(?:\/([^\#\/?]+))?(\#[\s\S]*)?$/);
 	if(!result) throw new Error(url);
 	let artist = result[1];
 	let urlType = result[2];
@@ -39,23 +39,35 @@ while(urlPool.length>0) {
 	let hash = result[4];
 	// console.log(url,artist,urlType,id);
 	switch (urlType) {
-		case "album":
+		case "artists":{// this is a label
+			let r = await fetch(`https://${artist}.bandcamp.com/artists`);
+			let string = await r.text();
+			let result = string.match(/https:\/\/[^\.]+\.bandcamp\.com\?label=/g);
+			if(!result) throw dump(url);
+			let artists = result.filter((v:string,i:number,a:string[])=>a.indexOf(v)===i).map((t)=>t.slice(0,-7)+`#label=${artist}`);
+			urlPool.push(...artists);
+		}
+		break;
+		case "album":{
 			let r = await fetch(`https://${artist}.bandcamp.com/album/${id}`);
 			let string = await r.text();
 			let result = string.match(/track\/[^\"?&#]+/g);
 			if(!result) throw dump(url);
-			let tracks = result.filter((v:string,i:number,a:string[])=>a.indexOf(v)===i).map((t)=>`https://${artist}.bandcamp.com/${t}#album=${id}`);
+			let tracks = result.filter((v:string,i:number,a:string[])=>a.indexOf(v)===i).map((t)=>`https://${artist}.bandcamp.com/${t}#album=${id}`+hash);
 			urlPool.push(...tracks);
+		}
 		break;
 		case "track":{
+			let label = "unknown";
 			let album = "unknown";
 			let array = hash.slice(1).split("#");
 			for(let i=0; i<array.length; i++) {
 				if(array[i].startsWith("album=")) album = array[i].slice(6).replace(/-/g," ");
+				if(array[i].startsWith("label=")) label = array[i].slice(6).replace(/-/g," ");
 			}
 
 			let track = id.replace(/-/g," ");
-			let path = pattern.replace(/{artist}/g,artist).replace(/{album}/g,album).replace(/{track}/g,track)+".mp3";
+			let path = pattern.replace(/{label}/g,label).replace(/{artist}/g,artist).replace(/{album}/g,album).replace(/{track}/g,track)+".mp3";
 			if(await exists(path)) {
 				console.log("skipping:",path);
 				continue;
@@ -81,24 +93,26 @@ while(urlPool.length>0) {
 			let results = string.match(/(?:track|album)\/[^\"]+/g);
 			if(!results) throw dump(url,string);
 			for(let i=0; i<results.length; i++) {
-				urlPool.push(`https://${artist}.bandcamp.com/${results[i]}`);
+				urlPool.push(`https://${artist}.bandcamp.com/${results[i]}`+hash);
 			}
 		}
 		break;
 	}
+	let labels = 0;
 	let artists = 0;
 	let albums = 0;
 	let tracks = 0;
 	for(let i=0; i<urlPool.length; i++) {
-		let result = urlPool[i].match(/^https?:\/\/([^.]+).bandcamp.com\/?(?:(album|track)\/([^#\/?]+))?(\#[\s\S]*)?$/);
-		if(!result) throw dump(url);
+		let result = urlPool[i].match(/^https?:\/\/([^.]+)\.bandcamp\.com\/?(artists|album|track)?(?:\/([^\#\/?]+))?(\#[\s\S]*)?$/);
+		if(!result) throw dump(urlPool[i]);
 		let artist = result[1];
 		let urlType = result[2];
 		let id = result[3];
+		if(urlType === "artists") labels++;
 		if(urlType === undefined) artists++;
 		if(urlType === "album") albums++;
 		if(urlType === "track") tracks++;
 	}
-	console.log(`[todo] artists: ${artists}, albums: ${albums}, tracks: ${tracks}`);
+	console.log(`[todo] labels: ${labels} artists: ${artists}, albums: ${albums}, tracks: ${tracks}`);
 	await sleep(500);
 }
